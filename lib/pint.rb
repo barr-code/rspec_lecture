c6e2d80b73be28961b19d9faa481736fe4059648
Victoria Barr <vb329@georgetown.edu>
2014-11-02 19:55:12 +0000

    Ripped pint spec and class from Hadi's original

---
+++ lib/pint.rb
@@ -0,0 +1,25 @@
+class Pint
+
+attr_accessor :name
+
+def initialize(name = "carling")
+	@name = name
+	end
+
+def broken?
+	@broken = false
+end
+
+def full?
+	@full = true
+end
+	
+def self.stella
+	new("stella")
+end
+
+def self.kronenberg
+	new("kronenberg")
+  end
+
+end
---
+++ spec/pint_spec.rb
@@ -0,0 +1,31 @@
+require 'pint'
+
+describe Pint do
+
+let(:pint){Pint.new("stella")}
+let(:punter){double :punter}
+
+	it "should not be broken" do
+		expect(pint).not_to be_broken
+	end
+
+	it "should be full" do
+		expect(pint).to be_full
+	end
+
+
+	it "should have a name" do
+		lager = Pint.new("stella")
+		expect(lager.name).to eq ("stella")
+	end
+
+	it "should be a stella pint" do
+		stella = Pint.stella
+		expect(stella.name).to eq ("stella")
+	end
+
+	it "should be a Kronenberg" do
+		kronenberg = Pint.kronenberg
+		expect(kronenberg.name).to eq ("kronenberg")
+	end
+end
